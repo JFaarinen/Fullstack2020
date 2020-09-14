1ac60893cfe48d95c2b961846edd6475a26b1286
Juho F <juho.faarinen@gmail.com>
2020-09-15 00:13:10 +0300

    Osa6Teht19

--- osa6/anekdootit-redux/src/components/Notification.js
+++ osa6/anekdootit-redux/src/components/Notification.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
-const Notification = () => {
-  const notification = useSelector(state => state.notifications.notification);
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+  console.log('PROPSIT', props);
 
-  if (notification === undefined) {
+  if (props.notification === undefined) {
     return null;
   } else {
     return (
       <div style={style}>
-        {notification}
+        {props.notification}
       </div>
     )
   }
 }
 
-export default Notification;
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notifications.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification);
+
+export default ConnectedNotification;
